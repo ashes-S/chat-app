5f9c686c0b842ae24967f1c945cfebb7fa1beb84
ashes-S <65926031+ashes-S@users.noreply.github.com>
2022-02-02 18:04:43 +0530

    fix: prevent blank messages from being sent

--- js/client.js
+++ js/client.js
@@ -7,7 +7,7 @@ var joined = new Audio("join.wav");
 var disconnected = new Audio("left.wav");
 var msgTone = new Audio("msg.wav");
 
-const userJoin = (user_name, status) => {
+const userStatus = (user_name, status) => {
     const joinMsg = document.createElement("div");
     joinMsg.classList.add("middle", "user-joined", "msg-style");
     joinMsg.innerText = `${user_name} has ${status}`;
@@ -44,18 +44,21 @@ if (user_name != "") {
 //joining notification
 socket.on('user-joined', user_name => {
     let status = "joined";
-    userJoin(user_name, status);
+    userStatus(user_name, status);
 })
 
 //msg send
 form.addEventListener("submit", (e) => {
     e.preventDefault();
     let msg = msgInput.value;
-    let pos = "right";
-    let nmaewa = "nothing";
-    msgInput.value = "";
-    statusMsg(msg, pos, nmaewa);
-    socket.emit('send', msg);
+    if (msg.trim() == "") { }
+    else {
+        let pos = "right";
+        let nmaewa = "nothing";
+        msgInput.value = "";
+        statusMsg(msg, pos, nmaewa);
+        socket.emit('send', msg);
+    }
 })
 
 //msg receive
@@ -69,5 +72,8 @@ socket.on('receive', data => {
 //work with disconnecting users
 socket.on('user-left', user_name => {
     let status = "disconnected";
-    userJoin(user_name, status);
+    userStatus(user_name, status);
 })
+
+
+//make it send media files
